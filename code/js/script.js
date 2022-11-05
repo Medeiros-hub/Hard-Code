d1818ac7ec378881e44715142ceb376e8d277902
Medeiros-hub <79912057+Medeiros-hub@users.noreply.github.com>
2022-11-05 00:16:59 -0300

    feat: finished main page

--- code/js/script.js
+++ code/js/script.js
@@ -1,32 +1,66 @@
 let time = 2000,
     currentImgIndex = 0,
-    images = document
+    heroImages = document
         .querySelectorAll(".athlete-slider .item"),
-    max = images.length
+    max = heroImages.length,
+    heroName = document
+        .querySelectorAll('main .hero-section .hero-name'),
+    heroDescription = document
+        .querySelectorAll('.hero-section .hero-description p'),
+    heroBars = document
+        .querySelector('.bars-group .main-bar')
 
-let heroName = document.
-        querySelectorAll('main .hero-section .hero-name')
+
+function removeSelected(propertie) {
+    propertie[currentImgIndex]
+        .classList.remove("selected")
+}
+
+function addSelected(propertie) {
+    propertie[currentImgIndex]
+        .classList.add("selected")
+}
 
 function nextImg() {
-    images[currentImgIndex]
-        .classList.remove("selected")
-    heroName[currentImgIndex]
-        .classList.remove("selected")
+
+    removeSelected(heroImages)
+    removeSelected(heroName)
+    removeSelected(heroDescription)
+    /*removeSelected(heroBars)*/
+    
+    if (currentImgIndex == 0) {
+        heroBars.style.left = 0
+    } else if (currentImgIndex == 1) {
+        heroBars.style.left = '21.5%'
+    } else {
+        heroBars.style.left = '45%'
+    }
+
     currentImgIndex++
     
     if (currentImgIndex >= max) {
         currentImgIndex = 0
     }
 
-    images[currentImgIndex]
-        .classList.add("selected")
-    heroName[currentImgIndex]
-        .classList.add("selected")
+    addSelected(heroImages)
+    addSelected(heroName)
+    addSelected(heroDescription)
+    /*addSelected(heroBars)*/
 }
 
 
 
 function startImg() {
+    setTimeout(() => {
+        for (let i = 0; i < max; i++) {
+            heroImages[i]
+                .classList.remove("selected")
+            heroName[i]
+                .classList.remove("selected")
+            heroDescription[i]
+                .classList.remove("selected")
+        }
+    }, 1000)
     setInterval(() => {
         nextImg()
     }, time)
